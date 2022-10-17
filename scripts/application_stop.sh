485d0a8770659f74a96f7b84b671b28c83429f2a
Olivier BATTINI <olivier@olivierbattini.fr>
2022-10-17 15:36:54 +0200

    ci: add configuration for ec2 code deploy
    add appspec.yml and scripts for
    aws code deploy / ec2 instances
    
    add .env.example

---
+++ scripts/application_stop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script parameters
+
+LOGS_DIR="/home/logs"
+
+# Script logging
+
+exec > >(tee "$LOGS_DIR/application_stop.log") 2>&1
+
+# Stopping existing node servers
+
+echo "[APPLICATION_STOP] Stopping any existing node servers"
+pidof node
+RESULT=$?
+
+if test $RESULT -eq 0
+then
+    echo "[APPLICATION_STOP] Found node process, stopping node"
+else
+    echo "[APPLICATION_STOP] No node process found"
+    pkill node
+fi
+
+echo "[APPLICATION_STOP] Done"
---
+++ scripts/before_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script parameters
+
+APP_NAME="app"
+USER="ec2-user"
+INSTALL_DIR="/home/${USER}/${APP_NAME}"
+LOGS_DIR="/home/logs"
+
+# Script logging
+
+exec > >(tee "$LOGS_DIR/before_install.log") 2>&1
+
+# Download & install node version manager
+
+echo "[BEFORE_INSTALL] Downloading Node Version Manager"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+
+echo "[BEFORE_INSTALL] Installing Node LTS version"
+nvm install --lts
+
+# Create working directory if it does not exist
+
+if [ -d "$INSTALL_DIR" ]; then
+  echo "[BEFORE_INSTALL] Install directory already exists ${INSTALL_DIR}"
+else
+  echo "[BEFORE_INSTALL] Creating directory ${INSTALL_DIR}"
+  mkdir ${INSTALL_DIR}
+fi
+
+echo "[BEFORE_INSTALL] Done"
